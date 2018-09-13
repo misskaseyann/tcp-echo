88a3c4a062a5fb5f5f3bef73f04ad7b0adfd5197
Kasey Ann <kaseystowell@Kaseys-MacBook-Pro.local>
2018-09-13 19:24:08 -0400

    final commenting

--- proj/tcpfileclient.java
+++ proj/tcpfileclient.java
@@ -3,21 +3,25 @@ import java.net.*;
 import java.nio.*;
 import java.nio.channels.*;
 
+/*
+ * TCP file transfer, client side.
+ */
 class tcpfileclient {
 	public static void main(String args[]) {
 		try {
+			// Open socket channel.
 			SocketChannel sc = SocketChannel.open();
 			Console cons = System.console();
-
 			boolean isint = false;
 			int p = 0;
+
+			// Ask for an IP address.
 			String a = "127.0.0.1";
-			String m = cons.readLine("Give an address: ");
+			String m = cons.readLine("Give an IP address: ");
 			if (m.matches("(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)")) a = m;
 			else System.out.println("Not an address. Using default.");
 
-			System.out.println(a);
-
+			// Ask for a port number.
 			while(!isint) {
 				m = cons.readLine("Give a port number: ");
 				if (m.matches("^-?\\d+$")) { 
@@ -26,23 +30,26 @@ class tcpfileclient {
 				} else System.out.println("Not a number.");
 			}
 
+			// Connect.
 			sc.connect(new InetSocketAddress(a, p));
 
-			m = cons.readLine("Enter your message: ");
+			// Get message and file type.
+			m = cons.readLine("Enter filepath: ");
 			String type = "";
 			String[] arr = m.split("\\.");
 			if (arr.length > 0) {
 				type = arr[arr.length - 1];
 			}
-
+			// Send message data to server.
 			ByteBuffer buf = ByteBuffer.wrap(m.getBytes());
 			sc.write(buf);
 
+			// Get file data from server.
 			ByteBuffer buf2 = ByteBuffer.allocate(5000);
 			int bytesRead = sc.read(buf2);
 			FileOutputStream bout = new FileOutputStream("download." + type);
 			FileChannel sbc = bout.getChannel();
-
+			// Read until there is no more data.
 			while(bytesRead != -1) {
 				buf2.flip();
 				sbc.write(buf2);
@@ -50,6 +57,7 @@ class tcpfileclient {
 				bytesRead = sc.read(buf2);
 			}
 
+			// Finished. Downloaded file in same folder as this.
 			System.out.println("Downloaded file!");
 			sc.close();
 		} catch(IOException e) {
